15d941d24a4ea70e1fe45cd25f0ea144c71dbd10
cold_blade <cold_blade@126.com>
2017-05-06 19:04:54 +0800

    add a class

---
+++ src/com/blade/connection/BladeSocketInputStream.java
@@ -0,0 +1,17 @@
+package com.blade.connection;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+/**
+ * @author: cold_blade
+ * @date: 2017年5月6日
+ * @version 1.0
+ */
+public final class BladeSocketInputStream extends BufferedInputStream {
+
+  public BladeSocketInputStream(InputStream in) {
+    super(in);
+    // TODO Auto-generated constructor stub
+  }
+}
